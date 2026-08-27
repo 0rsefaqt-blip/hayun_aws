e5cd1de31a2336b51c901aa855b7473d89cb01eb
byunhayun <0.rsefaqt@gmail.com>
2026-08-27 18:15:46 +0900

    0827

---
+++ spring boot/community/src/main/java/kr/fast/community/entity/Board.java
@@ -0,0 +1,31 @@
+package kr.fast.community.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "board")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Board {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)//DB에서 AUTO_INCREMENT로 기본키 생성
+	int id;
+	
+	@Column(name="name")
+	String name;
+
+}
