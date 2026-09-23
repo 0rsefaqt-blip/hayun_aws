5c7fb46896fe0266b347731d06d71dfd6e92d073
byunhayun <0.rsefaqt@gmail.com>
2026-09-23 11:13:08 +0900

    스프링 기본 틀 생성

---
+++ spring boot/jejuro/src/main/java/kr/fast/jejuro/JejuroApplication.java
@@ -0,0 +1,13 @@
+package kr.fast.jejuro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JejuroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JejuroApplication.class, args);
+	}
+
+}
